2120f5a87de9a9a14c8a7fde1b1f9bee3014f830
1337x <71149118+r1337x@users.noreply.github.com>
2024-02-14 14:55:39 +0000

    Add MasterCard Currency Conversion
    Added currency conversion using MasterCard API

--- plugins/utils/currency/__main__.py
+++ plugins/utils/currency/__main__.py
@@ -8,66 +8,155 @@
 #
 # All rights reserved.
 
-import json
+import asyncio
+from argparse import ArgumentError, ArgumentParser
+from typing import Optional
 
 import aiohttp
 from emoji import get_emoji_regexp
-
-from pyrogram import enums
-
-from userge import userge, Message
-from .. import currency
+from userge import Message, userge
 
 CHANNEL = userge.getCLogger(__name__)
 LOG = userge.getLogger(__name__)
 
 
-@userge.on_cmd("cr", about={
-    'header': "use this to convert currency & get exchange rate",
-    'description': "Convert currency & get exchange rates.",
-    'examples': "{tr}cr 1 BTC USD"})
-async def cur_conv(message: Message):
+class FallableArgumentParser(ArgumentParser):
+    def exit(self, status: int = 0, message: Optional[str] = None):
+        raise ArgumentError(None, message or "")
+
+    def error(self, message: str):
+        self.exit(2, message)
+
+
+parser = FallableArgumentParser(
+    ".cr",
+    usage="%(prog)s FROM TO [AMOUNT]",
+    description="Convert currency & get exchange rates.",
+    add_help=False,
+)
+
+CURRENCIES = {}
+
+
+@userge.on_start
+async def init() -> None:
+    for _ in range(1):
+        if len(CURRENCIES) == 0:
+            async with aiohttp.ClientSession() as session, session.get(
+                "https://www.mastercard.us/settlement/currencyrate/settlement-currencies"
+            ) as response:
+                if response.status_code != 200:
+                    break
+
+                res = await response.json()
+                data = res["data"]["currencies"]
+                for currency in data:
+                    CURRENCIES[currency["alphaCd"]] = currency["currNam"]
+
+    parser.add_argument(
+        "from_currency",
+        metavar="FROM",
+        choices=CURRENCIES.keys() if len(CURRENCIES) > 0 else None,
+        help="Input/Source Currency",
+    )
+    parser.add_argument(
+        "to_currency",
+        metavar="TO",
+        choices=CURRENCIES.keys() if len(CURRENCIES) > 0 else None,
+        help="Desired Currency",
+    )
+    parser.add_argument(
+        "amount",
+        type=float,
+        metavar="AMOUNT",
+        default=1,
+        nargs="?",
+        help="Amount for input currency (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-d",
+        "--date",
+        action="append",
+        default=["0000-00-00"],
+        help="Date to use while fetching rates,\nMultiple args are supported by repeating -d DATE",
+    )
+
+
+@userge.on_cmd(
+    "cr",
+    about={
+        "header": "use this to convert currency & get exchange rate",
+        "description": "Convert currency & get exchange rates.",
+        "examples": "{tr}cr BTC USD 1",
+    },
+)
+async def currency_conversion(message: Message):
     """
     this function can get exchange rate results
     """
-    if currency.CURRENCY_API is None:
-        await message.edit(
-            "<code>Oops!!get the API from</code> "
-            "<a href='https://free.currencyconverterapi.com'>HERE</a> "
-            "<code>& add it to Heroku config vars</code> (<code>CURRENCY_API</code>)",
-            disable_web_page_preview=True,
-            parse_mode=enums.ParseMode.HTML, del_in=0)
+
+    filterinput = get_emoji_regexp().sub("", message.input_str)
+    await message.edit("`Processing...`")
+
+    args = filterinput.split()
+    try:
+        parsed_args = parser.parse_args(args)
+    except ArgumentError as e:
+        await message.err(f"`{parser.format_help()}\n{str(e)}`", del_in=5)
         return
 
-    filterinput = get_emoji_regexp().sub(u'', message.input_str)
-    curcon = filterinput.upper().split()
+    # fxDate	           YYYY-MM-DD	        The date of the transaction
+    # transCurr	           ISO Currency Code	3-digit ISO Currency Code of the transaction
+    # crdhldBillCurr	   ISO Currency Code	3-digit ISO Currency Code of the cardholder
+    #                                            billing currency
+    # transAmount	       Number	            Amount in transaction currency
+    # bankFee	           Number	            Percentage bank fee imposed by the issuer.
+    #                                            Accommodates 4 decimal points to the right
+    urls = (
+        (
+            "https://www.mastercard.us/settlement/currencyrate/conversion-rate?"
+            "bankFee=0"
+            f"&fxDate={date}"
+            f"&transCurr={parsed_args.from_currency}"
+            f"&crdhldBillCurr={parsed_args.to_currency}"
+            f"&transAmt={parsed_args.amount}",
+            date,
+        )
+        for date in parsed_args.date
+    )
 
-    if len(curcon) == 3:
-        amount, currency_to, currency_from = curcon
-    else:
-        await message.err("you entered invalid data")
-        return
+    msg = "--**Currency Exchange Rates**--\n\n"
 
-    if amount.isdigit():
-        url = ("https://free.currconv.com/api/v7/convert?"
-               f"apiKey={currency.CURRENCY_API}&q="
-               f"{currency_from}_{currency_to}&compact=ultra")
-        async with aiohttp.ClientSession() as ses, ses.get(url) as res:
-            data = json.loads(await res.text())
-        try:
-            result = data[f'{currency_from}_{currency_to}']
-        except KeyError:
-            LOG.info(data)
-            await message.edit("`invalid response from api !`", del_in=5)
-            return
-        result = float(amount) / float(result)
-        result = round(result, 5)
-        await message.edit(
-            "**CURRENCY EXCHANGE RATE RESULT:**\n\n"
-            f"`{amount}` **{currency_to}** = `{result}` **{currency_from}**")
-        await CHANNEL.log("`cr` command executed sucessfully")
+    try:
+        async with aiohttp.ClientSession() as session:
+            for url, date in urls:
+                async with session.get(url) as response:
+                    if response.status_code != 200:
+                        await message.err(
+                            f"`Failed to retrieve data, status code: {response.status_code}`",
+                            del_in=5,
+                        )
+                        LOG.info(f".cr API Response: {await response.text()}")
+                        return
 
-    else:
-        await message.edit(
-            r"`This seems to be some alien currency, which I can't convert right now.. (⊙_⊙;)`",
-            del_in=0)
+                    res = await response.json()
+                    data = res["data"]
+                    if res.get("type") == "error":
+                        await message.err(
+                            f"`{data.get('errorMessage')}, status code: {data.get('errorCode')}`",
+                            del_in=5,
+                        )
+                        return
+
+                    msg += "__{}__:\n  {} **{}** = {} **{}**\n\n".format(
+                        date,
+                        *map(
+                            data.get, ["transAmt", "transCurr", "crdhldBillAmt", "crdhldBillCurr"]
+                        ),
+                    )
+
+                    await asyncio.sleep(1)
+    except Exception as e:
+        await message.err(f"`Unexpected Error Occured: {e}`")
+
+    await message.edit(msg)
