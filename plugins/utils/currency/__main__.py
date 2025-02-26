857b4feb0144e3fc8384122c2eb0d165a9e68b7f
ツ <71149118+r1337x@users.noreply.github.com>
2025-02-26 01:47:56 +0000

    Update __main__.py

--- plugins/utils/currency/__main__.py
+++ plugins/utils/currency/__main__.py
@@ -21,17 +21,16 @@ LOG = userge.getLogger(__name__)
 
 # Add headers constant
 HEADERS = {
-    "Host": "www.mastercard.us",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Cookie": "_abck=9D818DF69CE0B2308A74B221E86E03FF~0~YAAQbG0QAuusIpyTAQAA2N2ZqA2OExSUs9WJ9eKk/P7fh7UN0+bhamGggjA/2LnMzlt0aycpIlSFr0i3VkX0tx+MjaZVam+dOJN2eseq3sgrzLcfY6Upx1NWQoEHuXugy4V1F/NRnaRkEXEPFjVr22+0L1fXAq4pt6Zyj8Qc2PZp5KF8o1wyDQc2Ncrp5qp21JzlnaRZJmvGgzAviL7b3COUGqAixK7HccjlfdqDNdJTquUSNrv7pViYbF2BX/EyogKU1UxnHd7QqndNzsJ7ECmL3YQ5G34U4roQDUeCjQ3WbouXdg9/I51ABRgoBLBM474NUbKKzOjIW0AxjglF4gVwOeTMqMSnWuqhY0otT0yM4HCBoBtNI/a0YwFViKDXcfSXm4nqDDhrteCDoVx6wT+TrV1A7B1y3/do1zoTmc9mbvRPpSbR5kB4Oxvl3/EfWKBa9rEw3hyzR1Nm4j+XSH34WZbEK2ycU2zpHk9eSEkewuIglmqjPgaJyOz2D12s2wfijpAqiL+PK+jiu5cq3AsiZkAoSEjihiO4HcoouZC7xNO5ZMluLbzTkLRWikfIpGKsv7YoYAOtQsTVXgZpOlp474YkzJTa/7ocYptXQlztb1A4RluNPuBJydIcOOe/Uob3+hH+pcHBvv1F2XRgOEWTYbfs8v+ovcL5wmRMPpQ+cW5Mqqeo8u/Tab2l+81TtKp0zOfHl9NdQvKuR0/xYZ3KCVH9dH0AlPuUt+Qj/VDHxobYOS4ySj4qjQ==~-1~-1~-1",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Sec-GPC": "1"
+    'Host': 'www.mastercard.us',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Cookie': '_abck=D6C3C89210EAB2BD97B4F6A52424EFF8~0~YAAQbG0QAtFhYDuVAQAAZvvnPw03CoNjVUmJp8fdRlhBg0DiwRVnVuZgWNk6YJzeVWQpYtF3Eg/rB3ptqLCNzp+qrgyJ37v2ypGWgChJUV1iuPYpxx+e/IrqVxbO+IozC61xHOwH+nZEZj4cS6nRTzlP9wXw5tRMXBOYCjut2NGRcK1M4kzHor9DFPQeCOrnF/L0A9DVB/mw4LyIdGaT5vYGf6WwFShPM06O9OqwP1OPdZLYD76j35A0UH+/o9KJqEfPMS49S4N4SHuYMYuxU8dcPC6QmUXRSKic4i9QVDw/gbEs+LcV/vT0AYvepFhTCIPS4CFahjEqbd+h7dLVN6EwlBwSfQ9MjQwQc+97Vx/j/+xyN6t7G1DYlNK/t1Gkl5MB1fe1rZTPYGHESZUKYwDthZNJCTCKp+SweUJq2RJQ6CI5NcvNi2JfQrQs4GXkpll0smOPi/9O/xiV7xv4+rPn7Wb7LVynRa4aucaaEq+7U1wvvpYoFFBWor0kfvzfRy0iTOpSjK9s4l2oOmxh3O8DjS5mNnyDp0XRQ7TvjTxNxcFmmCVv/XSx0eTdSU9sD5DhNhx9Rz1YampSQWcWRzqKpRjNYNVZFsX8pVXnaa5h9bF4NOrnhUaKC+4pSt0iq1O6of3upZ0jF7aYxhOK7U52HE4Ig0TzOAeS17KS3q3c14iZYQVUV0DQyKXmE1eO6iKmc+lhytWfXkDIH/BS0GQyn8YnFDgUr9hZg3nnwmVJtC4QTSd6eMa5I3qz/4VrhM8G3ZfqHu0LEyFG3WZ7ILxY820i5NwlYUJaJ0PDg+1LCppq+uK4d+dz7bR86mJx~-1~-1~-1; ak_bmsc=394A716C8870A124600369360574442E~000000000000000000000000000000~YAAQbG0QAllfYDuVAQAATHvmPxpkrQYOrN2UgsGhi4Rc7xr0iWKMFDW7h4Zo280uJH63bE/5UY0dk21gxN8kogc1jl9AVFAA6d8s93msG8GNfnklK4TfMIxP9sbGmmWM7FPiCMPXta8dy/OJ1X+zx87h17peAhuzj2eRj4Umw8A2jg+Cja65eM6N/8wdaRAreal5aoTMCunBJcj1HI0AQS+09j71GeF9bfTOR0EGY9Q7yppzTp95xt4cUe/iEy+pGvb4dux6bqBTReQj0BropGHsR8WqUYc15CjLkIVgaycFK0j0KSPVuna20nhRaJOQL57pkJYSERv4aNqpt6GOV23JMb9PNzskc22F0nBFtNfvN5nh6mtUbsDM0nvOIw71fwfm4bvQ4foKEpxLnlI=; bm_sz=13F766DCC57AFABF987F0BE80E6F8819~YAAQbG0QAtNhYDuVAQAAZvvnPxrCjTpyma+ohVliatEV3ibv/MUuqYcjxIjlNTswZY0w70c4rLsGda28xALrzHcQMxG1MEW1e/pfYVDsKC/hrldMsadWf1KWPhbIHnti55QH2TdaDaFy9IcTPa4RTgo2XzbEcL1zu1E6K1sgnKwQ6m+mbwGAcfD449D+Y0ODDzzmYSr7jldrusUC+S2KJw+j9QoDXnINBcd2EFkFr07g79jTw6O0bG0dihUwrpM1pgw3w5SP5Yb468UNiE7NtduUR5T8j9l2oqzwjxvqwZ8FI1o/1mu1ymLG2xNIpMPOwijhWK4BcdoYuHpnoJTNZBKIjtvOiS2vXNws7rYHzVKQB3vlpjsKQpjANx0VWkoUTdJhTPiQ934kYukZhvwHt9ga65C0xw6WoYnswEUwyCOdbLIH7g==~3490613~3749954; BIGipServerwww.dm.mc.global-https-pool=!OF1JfZMfHkAGJ+y6oJ35j37UF/50ILn05SYqipnxjDwjNNDMZmbICPmrM7GGmhhNmfc2iTFZcih4Fk4=; TS013559a7=019243af6fc6d6211a994384e7c6f0c9a6580019395a238d70b9611c523bfe00b772e347c7a3757325f6b5628ee89a50d0f3bce9dd; bm_sv=D7BCE826D598445EE315D0ED999493B1~YAAQbG0QAtJhYDuVAQAAZvvnPxpfoqs+5NsOqTkMX1TOlVObWHOnhTM74cT22o2SPIg2I5PsL2omwzrD5KbCKX+kl2yi9Nsks5v5TKo8ZLmqVnUmc/NWWffTS4d9vg8n8cXLKxQDAc09M6ozx72t12GUFDiibJxLWWlcV9CST2DT0MHie9BHDbsWNoMg00jBtgAy5FApGbiTR/URexuWw0Xqjo2sn8bKvVLk9rJSkkGl3bAmb6ET3oo4AjLJZyQhpdb6~1',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site',
+    'Sec-GPC': '1'
 }
 
 class FallableArgumentParser(ArgumentParser):
